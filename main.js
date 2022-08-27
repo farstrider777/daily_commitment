6eedb6783ae9cd54fd40507cbb3bee3d7d64be5d
farstrider777 <jjyet@yahoo.com>
2022-08-27 15:31:11 -0400

    added more code for counter

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ console.log(x)
 
 var visitCount = localStorage.getItem("page_view");
 console.log(visitCount)
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+
+console.log(visitCount)
